7d3128bbd0beb8769b554056748d2a83cf92f276
Krishna Kumar~ <krishnakumar050.kk@gmail.com>
2025-05-06 13:43:14 +0530

    server.js fixed

--- Practise/server.js
+++ Practise/server.js
@@ -11,7 +11,6 @@ const forumPost = require("./routes/postRoutes");
 const notification = require("./routes/notificationRoutes");
 const refreshToken = require("./routes/refreshTokenRoutes");
 const productroutes = require("./routes/e-commerce routes/productroutes");
-const ordersRoutes = require("./routes/e-commerce routes/orderRoutes");
 
 // Connect to the database
 connectDb();
@@ -23,21 +22,22 @@ app.use(express.json());
 
 const cors = require("cors");
 
-const corsOptions = {
-  origin: [
-    "https://comicplane.site",
-    "https://www.comicplane.site",
-    "http://comicplane.site",
-    "http://localhost:5173",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
+app.use(
+  cors({
+    origin: [
+      "https://comicplane.site",
+      "http://localhost:5173",
+      "http://comicplane.site",
+      'https://www.comicplane.site'
+    ], // Allow frontend origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these HTTP methods
+    credentials: true, // Allow cookies and authentication
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow headers that might be included in the request
+  })
+);
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // <-- use same config
-
+// ✅ Ensure OPTIONS request is handled properly
+app.options("*", cors());
 
 const port = process.env.PORT || 5000; // Default to 5000 if no PORT environment variable is set
 
@@ -45,7 +45,6 @@ const port = process.env.PORT || 5000; // Default to 5000 if no PORT environment
 app.use("/api/contacts", contactsRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productroutes);
-app.use("/api/orders", ordersRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/comments", commentRouter);
 app.use("/api/like", likeRouter);
